1d62356891e764f5044f68ae55d56877d7a98de7
Elaina Martineau <elainamartineau@gmail.com>
2021-04-23 07:01:29 -0600

    Next path fix

--- next.config.js
+++ next.config.js
@@ -1,5 +1,12 @@
-module.exports = {
-    future: {
-      webpack5: true,
-    },
+let e = {
+  future: {
+    webpack5: true,
+  }
+};
+
+if (process.env.GITHUB_RUN_ID) {
+  e.assetPrefix = "/arachne/";
+  e.basePath = "/arachne";
 }
+
+module.exports = e;
