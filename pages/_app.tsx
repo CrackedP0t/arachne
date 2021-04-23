c98d60eb864152dea08f63167493fbab60247de9
Elaina Martineau <elainamartineau@gmail.com>
2021-04-23 07:55:16 -0600

    Use env variables

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,11 @@
 import type { AppProps /*, AppContext */ } from 'next/app'
-import "./sprites.css";
+
+if (process.env.GITHUB_RUN_ID) {
+    require("./gh_pages.css");
+}
+
 import "./global.css";
+import "./sprites.css";
 
 function Arachne({ Component, pageProps }: AppProps) {
     return <Component {...pageProps} />
